93e22e2d4e4fcb07d7012334f03d1b984ed5976a
Nicola Genesin <nicolagenesin@yahoo.it>
2013-07-31 03:37:43 +0200

    + dynamic camera
    + 1 finger to move.
    + 3(+) fingers using the second hand to stop camera movement.

--- Assets/Scripts/CameraLeap.cs
+++ Assets/Scripts/CameraLeap.cs
@@ -6,8 +6,13 @@ public class CameraLeap : MonoBehaviour {
 	public Leap.Controller controller;
 	public Leap.Listener listener;
 	public Leap.Frame guiFrame;
+	public Leap.Hand hand0;
+	public Leap.Hand hand1;
+	public int hand0ID;
+	public int hand1ID;
 	public string globalInfo;
 	public string old_globalInfo;
+	public bool hasPaused;
 	
 	// Use this for initialization
 	void Start () {
@@ -20,6 +25,8 @@ public class CameraLeap : MonoBehaviour {
         controller.EnableGesture(Leap.Gesture.GestureType.TYPEKEYTAP);
         controller.EnableGesture(Leap.Gesture.GestureType.TYPESCREENTAP);
         controller.EnableGesture(Leap.Gesture.GestureType.TYPESWIPE);
+		hasPaused=false;
+
 	}
 	
 	// Update is called once per frame
@@ -30,24 +37,36 @@ public class CameraLeap : MonoBehaviour {
 		if (!frame.Hands.Empty)
         {
             // Get the first hand
-            Leap.Hand hand = frame.Hands[0];
-
-            // Check if the hand has any fingers
-            Leap.FingerList fingers = hand.Fingers;
+            hand0 = frame.Hands[0];
+			hand1 = frame.Hands[1];
 			
-            if (!fingers.Empty && !frame.Hands[1].IsValid)
+            // Check if the hand has any fingers
+            Leap.FingerList fingers = hand0.Fingers;
+			
+            if (!fingers.Empty)
             {
 				Leap.Finger firstfinger = fingers[0];
-										
-				float moveInputX = firstfinger.TipVelocity.x /1000; 
-		        transform.position += new Vector3(moveInputX, 0, 0);
 				
-				float moveInputY = firstfinger.TipVelocity.y /1000; 
-		        transform.position += new Vector3(0, moveInputY, 0);
-			
-				float moveInputZ = firstfinger.TipVelocity.z /1000; 
-		        transform.position -= new Vector3(0, 0 , moveInputZ);
-					
+				// If at leasts 3 fingers are valid on the second hand (remember that when you take away your hand from leap,the second hand becomes the first on the HandsList) (*)<--
+				if ((hand1.Fingers.Count) >=3 ){
+					hasPaused=true;
+				}				
+				if (firstfinger.IsValid){
+					hasPaused=false;
+					if (!hand1.IsValid ||(hand1.IsValid && hand1.Fingers.Count<=2) ){
+						    if (hasPaused==false && frame.Hands[0].Id != hand1ID){  // (*)<--
+								float moveInputX = firstfinger.TipVelocity.x /600; 
+						        transform.position += new Vector3(moveInputX, 0, 0);
+								
+								float moveInputY = firstfinger.TipVelocity.y /600; 
+						        transform.position += new Vector3(0, moveInputY, 0);
+							
+								float moveInputZ = firstfinger.TipVelocity.z /600; 
+						        transform.position -= new Vector3(0, 0 , moveInputZ);
+							}	
+					}
+				}
+		
                 // Calculate the hand's average finger tip position
                 Leap.Vector avgPos = Leap.Vector.Zero;
                 foreach (Leap.Finger finger in fingers)
@@ -59,26 +78,20 @@ public class CameraLeap : MonoBehaviour {
                             + " fingers, average finger tip position: " + avgPos);
             }
 			
-			if (frame.Hands[1].IsValid){
-				Leap.Hand handSx = frame.Hands[1];
-				
-				if (handSx.Fingers.Count==4) transform.position = new Vector3(0,50,-50);
-				
-			}
-
-            // Get the hand's sphere radius and palm position
-            print("Hand sphere radius: " + hand.SphereRadius.ToString("n2")
-                        + " mm, palm position: " + hand.PalmPosition);
-
-            // Get the hand's normal vector and direction
-            Leap.Vector normal = hand.PalmNormal;
-            Leap.Vector direction = hand.Direction;
-
-            // Calculate the hand's pitch, roll, and yaw angles
-            print("Hand pitch: " + direction.Pitch * 180.0f / (float)3.14 + " degrees, "
-                        + "roll: " + normal.Roll * 180.0f / (float)3.14 + " degrees, "
-                        + "yaw: " + direction.Yaw * 180.0f / (float)3.14 + " degrees");
+//            // Get the hand's sphere radius and palm position
+//            print("Hand sphere radius: " + hand0.SphereRadius.ToString("n2")
+//                        + " mm, palm position: " + hand0.PalmPosition);
+//
+//            // Get the hand's normal vector and direction
+//            Leap.Vector normal = hand0.PalmNormal;
+//            Leap.Vector direction = hand0.Direction;
+//
+//            // Calculate the hand's pitch, roll, and yaw angles
+//            print("Hand pitch: " + direction.Pitch * 180.0f / (float)3.14 + " degrees, "
+//                        + "roll: " + normal.Roll * 180.0f / (float)3.14 + " degrees, "
+//                        + "yaw: " + direction.Yaw * 180.0f / (float)3.14 + " degrees");
         }
+		
 		
 		
 		
@@ -127,34 +140,6 @@ public class CameraLeap : MonoBehaviour {
                                    + ", position: " + swipe.Position
                                    + ", direction: " + swipe.Direction
                                    + ", speed: " + swipe.Speed);
-				
-							
-//							
-//					float moveInputx = swipe.Direction.x * Time.deltaTime * 3; 
-//			        transform.position += new Vector3(moveInputx, 0, 0);
-//					
-//					float moveInputy = swipe.Direction.y * Time.deltaTime * 3;
-//			        transform.position += new Vector3(0, moveInputy, 0);
-//					
-//					if (Input.GetKey (KeyCode.R)){ 
-//						float moveInputz = Time.deltaTime * 3; 
-//			        	transform.position += new Vector3(0, 0, moveInputz);
-//					}
-//					
-//					if (Input.GetKey (KeyCode.F)){ 
-//						float moveInputz = Time.deltaTime * 3; 
-//			        	transform.position += new Vector3(0, 0, -moveInputz);
-//					}
-//							
-//							
-//							
-							
-							
-							
-							
-							
-				
-				
                     break;
                 case Leap.Gesture.GestureType.TYPEKEYTAP:
                     Leap.KeyTapGesture keytap = new Leap.KeyTapGesture(gesture);
@@ -176,6 +161,14 @@ public class CameraLeap : MonoBehaviour {
             }
         }
 	
+		
+		if (!frame.Hands.Empty) {
+			hand0ID = hand0.Id;
+		    if ( hand1.Id != -1 ) hand1ID = hand1.Id; // since when u take away your first hand,the second one becomes the first one on the list (with ID = 1),I want to remember its value
+		}
+		else{
+			hand0ID = -1;
+		}
 	}
 	
 	void OnGUI()
@@ -187,13 +180,23 @@ public class CameraLeap : MonoBehaviour {
                     + ", hands: " + guiFrame.Hands.Count
                     + ", fingers: " + guiFrame.Fingers.Count
                     + ", tools: " + guiFrame.Tools.Count
-                    + ", gestures: " + guiFrame.Gestures().Count);
+                    + ", gestures: " + guiFrame.Gestures().Count
+					+ ", FirstHandID: " + hand0ID 
+					+ ", SecondHandID: " + hand1ID);
+
 		
-		GUILayout.Label(globalInfo);
     }  
 	
 	void Exit(){
 		controller.RemoveListener(listener);
         controller.Dispose();
 	}
+	
+	
+	IEnumerator Sleep() {
+		 Debug.Log("Before Waiting 2 seconds");
+		 yield return new WaitForSeconds(15);
+		 Debug.Log("After Waiting 2 Seconds");
+	}
+	
 }
